4bad20955ff5f20ce67923cfb6770a35416227f1
Renzo <renzochong@gmail.com>
2023-11-10 23:33:58 -0500

    Final code

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,10 +17,11 @@
 buildscript {
     extra.apply {
         set("nav_version", "2.5.3")
+        set("room_version", "2.5.2")
     }
 }
 plugins {
-    id("com.android.application") version "8.1.2" apply false
-    id("com.android.library") version "8.1.2" apply false
+    id("com.android.application") version "8.1.3" apply false
+    id("com.android.library") version "8.1.3" apply false
     id("org.jetbrains.kotlin.android") version "1.9.10" apply false
 }
